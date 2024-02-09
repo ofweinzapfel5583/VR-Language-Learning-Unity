8e41bf0b9961a81acb2af868fa4ac8c26541f235
J.A. Fisher <jadlerfisher@gmail.com>
2024-02-09 17:22:13 -0500

    Edits that demo Buttons working

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -7,11 +7,14 @@ public class TutorialManager : MonoBehaviour
 
     public void ContinueToCafeScene()
     {
+        Debug.Log("calling?");
+        //You don't want to do this. Instead you want to set the tutorial to not active.
         SceneManager.LoadScene("CafeScene");
     }
 
     public void GoBackToMainScene()
     {
+        Debug.Log("or you?");
         SceneManager.LoadScene("MainScene");
     }
 }
